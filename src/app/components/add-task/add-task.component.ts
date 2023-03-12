8c78bcced8e945eef442637d96504b5d17f9e054
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2023-03-12 17:43:09 -0600

    Version 1.0.0 finished

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,58 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  private dateToString(day: string): string {
+    return (
+      new Date(day).toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+      }) +
+      ' at ' +
+      new Date(day).toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+      })
+    );
+  }
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.dateToString(this.day),
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
